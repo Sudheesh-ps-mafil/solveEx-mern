dbd1ab92b8d31ea2611d798180cd7a229889e995
Sanjay9745 <sanjaysanthosh919@gmail.com>
2023-10-31 19:24:17 +0530

    env removed

--- frontend/src/config/SERVER_URL.js
+++ frontend/src/config/SERVER_URL.js
@@ -1,3 +1,3 @@
-// const SERVER_URL="http://localhost:3001/api"
-const SERVER_URL="https://rtouch.onrender.com/api"
+const SERVER_URL="http://localhost:3001/api"
+// const SERVER_URL="https://rtouch.onrender.com/api"
 export default SERVER_URL;
--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -7,17 +7,27 @@ import { GoogleAuthProvider, getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain:import.meta.VITE_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: import.meta.VITE_FIREBASE_DATABASE_URL,
+//   projectId: import.meta.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId:import.meta.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.VITE_FIREBASE_APP_ID,
+//   measurementId: import.meta.VITE_FIREBASE_MEASUREMENT_ID,
+// };
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain:import.meta.VITE_FIREBASE_AUTH_DOMAIN,
+  authDomain: "my-web-site-385808.firebaseapp.com",
   databaseURL: import.meta.VITE_FIREBASE_DATABASE_URL,
   projectId: import.meta.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId:import.meta.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  messagingSenderId: import.meta.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.VITE_FIREBASE_MEASUREMENT_ID,
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
+export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
