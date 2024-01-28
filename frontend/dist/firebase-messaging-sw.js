bb82bd8d0f5590c07e2ff3f293aa1b9f906e13d2
Sanjay9745 <sanjaysanthosh919@gmail.com>
2024-01-28 21:35:21 +0530

    firebase public addded

---
+++ frontend/dist/firebase-messaging-sw.js
@@ -0,0 +1,31 @@
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
+importScripts('https://www.gstatic.com/firebasejs/<v9+>/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/<v9+>/firebase-messaging-compat.js');
+// Initialize the Firebase app in the service worker by passing the generated config
+const firebaseConfig = {
+    apiKey: "AIzaSyB9W0_wxh7tUDHEvFULuxGeBErzfAPwM8w",
+    authDomain: "my-web-site-385808.firebaseapp.com",
+    databaseURL: "https://my-web-site-385808-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "my-web-site-385808",
+    storageBucket: "my-web-site-385808.appspot.com",
+    messagingSenderId: "503438361782",
+    appId: "1:503438361782:web:235f222f9624d95783d217",
+    measurementId: "G-0WV0EZY57Z"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log("Received background message ", payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
