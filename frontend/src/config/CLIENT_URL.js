df7043c4c2e21b4a3ef9dbeadb421fba175e6e98
Sanjay9745 <sanjaysanthosh919@gmail.com>
2024-02-22 22:26:17 +0530

    new commit

--- frontend/src/config/CLIENT_URL.js
+++ frontend/src/config/CLIENT_URL.js
@@ -1,4 +1,5 @@
 // const CLIENT_URL="http://localhost:4000"
-const CLIENT_URL="https://r-touch-app.vercel.app"
+// const CLIENT_URL="https://r-touch-app.vercel.app"
+const CLIENT_URL="https://rtouch.onrender.com"
 
 export default CLIENT_URL;
